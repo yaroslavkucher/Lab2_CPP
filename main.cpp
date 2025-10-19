10e583a0d4a7523c030a2d160a7eb2b24dbe0887
Yaroslav <kucheryaroslav2@gmail.com>
2025-10-20 01:25:52 +0300

    Visual design of the project

--- main.cpp
+++ main.cpp
@@ -4,8 +4,22 @@
 #include <algorithm>
 #include <chrono>
 #include <execution>
+#include <thread>
+#include <string>
+#include <format>
+#include <future>
 
-void wfile(std::string policy, std::chrono::duration<double, std::milli> duration)
+void general_info()
+{
+	std::cout << std::endl << ':' << std::string(70, '~') << ':' << std::endl;
+	std::cout << std::format(":{:^70}:", "The author of this program is Yaroslav Kucher K-27") << std::endl;
+	std::cout << std::format(":{:^70}:", "The program measures the time taken by the std::all_of algorithm.") << std::endl;
+	std::cout << std::format(":{:^70}:", "It tests different execution policies on vectors of random integers.") << std::endl;
+	std::cout << std::format(":{:^70}:", "Computer has " + std::to_string(std::thread::hardware_concurrency()) + " cores.") << std::endl;
+	std::cout << ':' << std::string(70, '~')<< ':'  << std::endl << std::endl;
+}
+
+void print_policy_time(std::string policy, std::chrono::duration<double, std::milli> duration)
 {
 	std::cout << "Policy: " << policy << ", Time taken: " << duration.count() << " ms" << std::endl;
 }
@@ -27,7 +41,7 @@ void policy_all_of(const std::vector<int>& vec)
 	auto end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double, std::milli> duration = end - start;
 
-	wfile("No policy", duration);
+	print_policy_time("No policy", duration);
 }
 
 template<typename ExecPolicy>
@@ -51,8 +65,13 @@ void policy_all_of(const std::vector<int>& vec, ExecPolicy&& policy)
 	auto end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double, std::milli> duration = end - start;
 
-	wfile(policy_name<ExecPolicy>(), duration);
+	print_policy_time(policy_name<ExecPolicy>(), duration);
 }
+
+/*std::chrono::duration<double, std::milli> parallel_all_of(const std::vector<int>& vec, int k)
+{
+
+}*/
 
 int main()
 {
@@ -60,6 +79,7 @@ int main()
 	std::uniform_int_distribution<int> distribution(0, 100);
 	std::vector<int> sizes = {100, 1'000, 10'000, 100'000, 1'000'000, 10'000'000};
 	int n = 1;
+	general_info();
 	for(auto vecSize: sizes)
 	{
 		std::cout << "Experiment #" << n++ << ":" << std::endl
@@ -73,12 +93,30 @@ int main()
 		{
 			std::cout << el << " ";
 		}*/
-		std::cout << std::endl;
 		policy_all_of(seqOfRandomNumbers);
 		policy_all_of(seqOfRandomNumbers, std::execution::seq);
 		policy_all_of(seqOfRandomNumbers, std::execution::unseq);
 		policy_all_of(seqOfRandomNumbers, std::execution::par);
 		policy_all_of(seqOfRandomNumbers, std::execution::par_unseq);
+		std::cout << std::string(53, '-') << std::endl
+			<< std::format("|{:^25}|{:^25}|", "K", "Duration") << std::endl;
+		auto best_duration = std::chrono::duration<double, std::milli>::max().count();
+		int K = 1;
+		for (int k = 1; k <= std::thread::hardware_concurrency() * 2; k = k * 2)
+		{
+			auto duration = 5;/*parallel_all_of(seqOfRandomNumbers, k);*/
+			if (duration < best_duration)
+			{
+				best_duration = duration;
+				K = k;
+			}
+			std::cout << std::string(53, '-') << std::endl
+				<< std::format("|{:^25}|{:^25}|", k, std::to_string(duration)+"ms") << std::endl;
+		}
+		std::cout << std::string(53, '-') << std::endl;
+		std::cout << "The best performance is achieved with " 
+			<< K << " part(s) - this is " << best_duration << " ms." << std::endl;
+		std::cout << std::endl << ':' << std::string(70, '~') << ':' << std::endl;
 	}
 	return 0;
 }
